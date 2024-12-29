ad467de8ff975c0665fa16e0a7cfdda70af7e9b9
Wiefran Varenzo <92115374+WiefranVarenzo@users.noreply.github.com>
2024-12-29 19:13:08 +0700

    Update grafik_ipkd.py

--- grafik_ipkd.py
+++ grafik_ipkd.py
@@ -5,29 +5,22 @@ import plotly.express as px
 
 def app():
     st.title('Aplikasi Visualisasi Data IPKD dan Prevalensi')
-
-    # BAGIAN IPKD #
     st.header("Bagian 1: Grafik IPKD (Garis dan Batang)")
-
-    # Upload file untuk IPKD
     st.subheader('Upload File untuk Grafik IPKD')
+    
     uploaded_ipkd_file = st.file_uploader("Upload File IPKD (Excel)", type=["xlsx"])
     if uploaded_ipkd_file is not None:
         df_ipkd = pd.read_excel(uploaded_ipkd_file, sheet_name='Hasil_Akhir')
     else:
-        df_ipkd = pd.read_excel('source/ipkd_results.xlsx')  # Default file
-
-    # Ubah nama kolom dan semua isi kolom menjadi huruf kapital
+        df_ipkd = pd.read_excel('source/ipkd_results.xlsx') 
+        
     df_ipkd.columns = df_ipkd.columns.str.upper()
     df_ipkd = df_ipkd.applymap(lambda s: s.upper() if type(s) == str else s)
-
     unique_provinces = df_ipkd['PROVINSI'].unique()
 
-    # Bagian 1.1: Grafik Garis - Peningkatan Nilai IPKD Tiap Tahun di Satu Daerah
     st.subheader("Grafik Garis: Peningkatan Nilai IPKD Tiap Tahun")
     provinsi = st.selectbox('Pilih Provinsi', unique_provinces, key='provinsi_selectbox')
 
-    # Filter kota/kabupaten berdasarkan provinsi
     filtered_cities = df_ipkd[df_ipkd['PROVINSI'] == provinsi]['KOTA/KABUPATEN'].unique()
     kota = st.selectbox('Pilih Kota/Kabupaten', filtered_cities, key='kota_selectbox')
 
@@ -35,13 +28,10 @@ def app():
                   'PENYAKIT TIDAK MENULAR', 'PENYAKIT MENULAR', 'SANITASI DAN KEADAAN LINGKUNGAN HIDUP']
 
     field = st.selectbox('Pilih Kolom', kolom_list, key='field_selectbox')
-
-    # Data untuk grafik garis
     data = df_ipkd[df_ipkd['KOTA/KABUPATEN'] == kota]
     tahun = data['TAHUN'].astype(str).str[:4].tolist()
     value = data[field].tolist()
 
-    # Plot grafik garis
     fig_line = go.Figure()
     fig_line.add_trace(go.Scatter(x=tahun, y=value, mode='lines+markers', name=field, line=dict(color='red', width=2)))
 
@@ -58,14 +48,10 @@ def app():
     )
 
     st.plotly_chart(fig_line)
-
-    # Bagian 1.2: Grafik Batang - Perbandingan Nilai IPKD Antar Daerah di Satu Tahun
     st.subheader("Grafik Batang: Perbandingan Nilai IPKD Antar Daerah")
 
     tahun_select = st.selectbox("Pilih Tahun", sorted(df_ipkd['TAHUN'].unique()), key='tahun_selectbox')
     provinsi_select = st.selectbox("Pilih Provinsi untuk Grafik Batang", unique_provinces, key='provinsi_bar_selectbox')
-
-    # Filter data berdasarkan provinsi dan tahun
     data_provinsi_tahun = df_ipkd[(df_ipkd['PROVINSI'] == provinsi_select) & (df_ipkd['TAHUN'] == tahun_select)]
 
     if not data_provinsi_tahun.empty:
@@ -98,45 +84,32 @@ def app():
         st.plotly_chart(fig_bar)
     else:
         st.write("Data tidak ditemukan untuk pilihan tahun dan provinsi tersebut.")
-
-    # BAGIAN PREVALENSI/PERSENTASE #
     st.header("Bagian 2: Grafik Persentase atau Prevalensi")
-
-    # Upload file untuk persentase
     st.subheader('Upload File untuk Grafik Persentase')
+    
     uploaded_percentage_file = st.file_uploader("Upload File Persentase (CSV)", type=["csv"])
     if uploaded_percentage_file is not None:
         df_percentage = pd.read_csv(uploaded_percentage_file)
     else:
-        df_percentage = pd.read_csv('source/datasetreal.csv')  # Default file
-
-    # Ubah nama kolom dan isi kolom menjadi huruf kapital
+        df_percentage = pd.read_csv('source/datasetreal.csv') 
     df_percentage.columns = df_percentage.columns.str.upper()
     df_percentage = df_percentage.applymap(lambda s: s.upper() if type(s) == str else s)
 
-    # Langkah 1: Pilih Provinsi untuk persentase
     unique_provinces_percentage = df_percentage['PROVINSI'].unique()
     provinsi_percentage = st.selectbox("Pilih Provinsi untuk Persentase", unique_provinces_percentage)
 
-    # Langkah 2: Pilih Kabupaten/Kota berdasarkan provinsi
     filtered_kabupaten = df_percentage[df_percentage['PROVINSI'] == provinsi_percentage]['KOTA/KABUPATEN'].unique()
     kabupaten = st.selectbox("Pilih Kabupaten/Kota", filtered_kabupaten)
-
-    # Langkah 3: Pilih kolom grafik
     options_for_graph = df_percentage.columns.tolist()
     options_for_graph.remove('PROVINSI')
     options_for_graph.remove('KOTA/KABUPATEN')
     options_for_graph.remove('TAHUN')
     pilihan_grafik = st.selectbox("Pilih Data untuk Ditampilkan", options_for_graph)
 
-    # Filter data berdasarkan pilihan pengguna
     df_filtered = df_percentage[(df_percentage['PROVINSI'] == provinsi_percentage) & (df_percentage['KOTA/KABUPATEN'] == kabupaten)]
 
-    # Langkah 4: Tampilkan grafik persentase/prevalensi
     if not df_filtered.empty:
         st.write(f"Menampilkan grafik {pilihan_grafik} untuk {kabupaten}, {provinsi_percentage}")
-
-        # Plot data dengan Plotly
         fig_prevalensi = px.line(df_filtered, x='TAHUN', y=pilihan_grafik, markers=True)
         fig_prevalensi.update_traces(line=dict(color='red', width=2))
         fig_prevalensi.update_layout(
